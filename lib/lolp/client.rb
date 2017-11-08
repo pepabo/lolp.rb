96ead14975f5741cafad6be203392868b1f79c4f
MORI Asami <nakano.a@pepabo.com>
2017-11-08 14:21:40 +0900

    Change instance variable to method

--- lib/lolp/client.rb
+++ lib/lolp/client.rb
@@ -3,20 +3,22 @@ require 'lolp/connection'
 module Lolp
   class Client
 
-    def initialize
-      @connection = Connection.new
-    end
-
     def get_projects
-      @connection.get('api/projects')
+      connection.get('api/projects')
     end
 
     def create_project(type, params = {})
-      @connection.post('api/projects', params.merge(haco_type: type))
+      connection.post('api/projects', params.merge(haco_type: type))
     end
 
     def delete_project(name)
-      @connection.delete("api/projects/#{name}")
+      connection.delete("api/projects/#{name}")
+    end
+
+    private
+
+    def connection
+      @connection ||= Connection.new
     end
   end
 end
