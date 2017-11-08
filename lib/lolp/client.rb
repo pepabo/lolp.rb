00c9cd9e624171b7d11254e4f8987994b7773347
MORI Asami <nakano.a@pepabo.com>
2017-11-08 13:03:28 +0900

    change simple API wrapper

--- lib/lolp/client.rb
+++ lib/lolp/client.rb
@@ -1,33 +1,22 @@
-require 'faraday'
-require 'faraday_middleware'
-require 'lolp/configuration'
+require 'lolp/connection'
 
 module Lolp
   class Client
-    include Lolp::Configuration
 
     def initialize
-      authorize
-      @default_client = generate_client
-    end
-    attr_reader :default_client
-
-    private
-
-    def generate_client
-      Faraday.new(url: config.api_url, headers: { cookie: @session }) do |faraday|
-        faraday.request :json
-        faraday.response :json, content_type: /\bjson$/
-        faraday.response :logger
-        faraday.adapter Faraday.default_adapter
-      end
+      @connection = Connection.new
     end
 
-    def authorize
-      unless @session
-        result = generate_client.post 'api/login', username: config.username, password: config.password
-        @session = result.env[:response_headers]['set-cookie']
-      end
+    def get_projects
+      @connection.get('api/projects')
+    end
+
+    def create_project(type, params = {})
+      @connection.post('api/projects', params.merge(haco_type: type))
+    end
+
+    def delete_project(name)
+      @connection.delete("api/projects/#{name}")
     end
   end
 end
---
+++ lib/lolp/connection.rb
@@ -0,0 +1,44 @@
+require 'faraday'
+require 'faraday_middleware'
+require 'lolp/configuration'
+
+module Lolp
+  class Connection
+    include Lolp::Configuration
+
+    def initialize
+      authorize
+      @connection = connection
+    end
+
+    def get(url)
+      @connection.get(url)
+    end
+
+    def post(url, params = {})
+      @connection.post(url, params)
+    end
+
+    def delete(url, params = {})
+      @connection.delete(url, params)
+    end
+
+    private
+
+    def connection
+      Faraday.new(url: config.api_url, headers: { Authorization: @token }) do |faraday|
+        faraday.request :json
+        faraday.response :json, content_type: /\bjson$/
+        faraday.response :logger
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+
+    def authorize
+      unless @token
+        result = connection.post('api/login', username: config.username, password: config.password)
+        @token = result.body
+      end
+    end
+  end
+end
