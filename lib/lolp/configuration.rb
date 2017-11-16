d23e51ce451b4614a1890436c23484c908670fcd
MORI Asami <nakano.a@pepabo.com>
2017-11-16 12:05:52 +0900

    Set default value for api_url

--- lib/lolp/configuration.rb
+++ lib/lolp/configuration.rb
@@ -12,7 +12,7 @@ module Lolp
       attr_accessor :api_url, :username, :password
 
       def initialize
-        @api_url  = ENV['LOLIPOP_MC_API_ENDPOINT']
+        @api_url  = ENV['LOLIPOP_MC_API_ENDPOINT'] || 'https://mc.lolipop.jp/api'
         @username = ENV['LOLIPOP_MC_USERNAME']
         @password = ENV['LOLIPOP_MC_PASSWORD']
       end
