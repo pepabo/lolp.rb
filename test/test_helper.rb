d67f48ac9ca070c232db29d6d3534fa97886f3f8
MORI Asami <nakano.a@pepabo.com>
2017-11-17 10:56:24 +0900

    Add test for projects

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,15 @@ require 'lolp'
 require 'minitest/autorun'
 require 'vcr'
 
+ENV['LOLIPOP_MC_API_ENDPOINT'] = 'https://staging.mc.lolipop.jp/api'
+
 VCR.configure do |config|
   config.cassette_library_dir = 'test/fixtures/vcr_cassettes'
   config.hook_into :faraday
+  config.filter_sensitive_data("Bearer <TOKEN>") do |interaction|
+    interaction.request.headers['Authorization'].first
+  end
+  config.ignore_request do |request|
+    URI(request.uri).path == '/api/login'
+  end
 end
