19eeaead079b0bffa8480fb93a46e97b6fbc74a4
MORI Asami <nakano.a@pepabo.com>
2017-11-17 09:31:44 +0900

    Add vcr gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "lolp"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'lolp'
+require 'minitest/autorun'
+require 'vcr'
 
-require "minitest/autorun"
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/fixtures/vcr_cassettes'
+  config.hook_into :faraday
+end
